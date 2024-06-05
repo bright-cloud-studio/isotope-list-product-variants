91089516ce17fb2f1a539884337d0e6afb5b0cf8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-05 12:13:48 -0400

    Create BcsListVariantsBundle.php

---
+++ src/BcsListVariantsBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
+ */
+
+namespace Bcs\ListVariantsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsListVariantsBundle extends Bundle
+{
+}
