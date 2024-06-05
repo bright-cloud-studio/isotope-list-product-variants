5a62080824053d823d85db8b7a2b274c34f097c8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-05 12:58:06 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,4 @@
 <?php
 
 // Frontend Modules
-$GLOBALS['FE_MOD']['isotope']['iso_list_ordered_products'] 	= 'Bcs\Module\ListOrderedProducts';
+$GLOBALS['FE_MOD']['isotope']['iso_list_product_variants'] 	= 'Bcs\Module\ListProductVariants';
