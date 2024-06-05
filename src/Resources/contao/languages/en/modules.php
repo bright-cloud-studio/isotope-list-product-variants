71e1029e6857f9a891a33e83ac3a4448c836fea1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-05 13:01:53 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['FMD']['iso_list_product_variants']    = array('List Product Variants1', 'A customized Isotope Order History module to list ordered products as a Product List module');
