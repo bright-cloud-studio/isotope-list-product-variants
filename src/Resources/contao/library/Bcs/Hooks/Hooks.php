151f018204a06ab597f15dbbc86e6841872fd136
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-11 14:17:01 -0400

    Update Hooks.php

--- src/Resources/contao/library/Bcs/Hooks/Hooks.php
+++ src/Resources/contao/library/Bcs/Hooks/Hooks.php
@@ -12,13 +12,12 @@ use Isotope\Model\ProductCollection\Cart;
 use Isotope\Model\ProductCollection\Order;
 
 
-class Hooks extends System {
+class Hooks extends System
 {
     protected static $arrUserOptions = array();
 
     /* HOOK - Triggered when trying to add a product to the cart on a Product Reader page */
     public function checkCollectionQuantity( Product $objProduct, $intQuantity, IsotopeProductCollection $objCollection ) {
-
         echo "HOOKY";
         die();
     }
