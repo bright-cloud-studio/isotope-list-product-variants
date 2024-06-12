12050fda1da3dd3e80a3f2709bc9f375bb03c77f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-12 11:57:25 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,4 +5,8 @@ $GLOBALS['FE_MOD']['bcs']['list_prod_var'] 	= 'Bcs\Module\ListProductVariants';
 
 /* Hooks */
 $GLOBALS['TL_HOOKS']['processFormData'][]      = array('Bcs\Hooks\Hooks', 'onProcessForm');
+
+
 $GLOBALS['ISO_HOOKS']['addProductToCollection'][] = array('Bcs\Hooks\Hooks', 'checkCollectionQuantity');
+//$GLOBALS['ISO_HOOKS']['updateItemInCollection'][] = array('Bcs\Hooks\Hooks', 'updateCollectionQuantity');
+//$GLOBALS['ISO_HOOKS']['copiedCollectionItems'][] = array('Bcs\Hooks\Hooks', 'mergeCollections');
